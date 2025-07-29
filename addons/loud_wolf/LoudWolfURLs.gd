728282087fe154fa1ae2fe2d41eabc4ad70c5b3b
angelator312 <112373005+angelator312@users.noreply.github.com>
2025-07-29 06:08:15 +0300

    added get_scores_by_player URL and added tests

--- addons/loud_wolf/LoudWolfURLs.gd
+++ addons/loud_wolf/LoudWolfURLs.gd
@@ -22,12 +22,17 @@ var remove_player_data:=api_url+"remove_player_data"
 #Scores:
 var save_score:=api_url+"save_score"
 var _get_scores:=api_url+"get_scores"
-var get_scores_by_player:=api_url+ "get_scores_by_player"
+var _get_scores_by_player:=api_url+ "get_scores_by_player"
 var get_top_score_by_player:=api_url+"get_top_score_by_player"
 var get_score_position:=api_url+"get_score_position"
 var get_scores_around:=api_url+"get_scores_around"
 var delete_score:=api_url+"delete_score"
 var wipe_leadeboard:=api_url+"wipe_leaderboard"
 
-func get_scores(maximum:int,ldboard_name:String,period_offset:int):
+
+func get_scores(maximum:int,ldboard_name:String,period_offset:int) -> String:
 	return LoudWolf.URLs._get_scores+"/" + str(LoudWolf.config.game_id) + "?max=" + str(maximum)  + "&ldboard_name=" + str(ldboard_name) + "&period_offset=" + str(period_offset)
+
+
+func get_scores_by_player(player_name: String, maximum: int, ldboard_name: String, period_offset: int) -> String:
+	return LoudWolf.URLs._get_scores_by_player+"/" + str(LoudWolf.config.game_id) + "?max=" + str(maximum)  + "&ldboard_name=" + str(ldboard_name.uri_encode()) + "&player_name=" + str(player_name.uri_encode()) + "&period_offset=" + str(period_offset)
--- addons/loud_wolf/modules/Scores/Scores.gd
+++ addons/loud_wolf/modules/Scores/Scores.gd
@@ -180,7 +180,7 @@ func get_scores_by_player_async(player_name: String, maximum: int=10, ldboard_na
 		SWLogger.info("Calling LoudWolf backend to get scores for player: " + str(player_name))
 		# resetting the latest_number value in case the first requests times out, we need to request the same amount of top scores in the retry
 		latest_max = maximum
-		var request_url = LoudWolf.URLs.get_scores_by_player+"/" + str(LoudWolf.config.game_id) + "?max=" + str(maximum)  + "&ldboard_name=" + str(ldboard_name.uri_encode()) + "&player_name=" + str(player_name.uri_encode()) + "&period_offset=" + str(period_offset)
+		var request_url =LoudWolf.URLs.get_scores_by_player(player_name,maximum,ldboard_name,period_offset)
 		LoudWolf.send_get_request(ScoresByPlayer, request_url)
 	return self
 
@@ -202,9 +202,6 @@ func _on_GetScoresByPlayer_request_completed(result, response_code, headers, bod
 			SWLogger.info("LoudWolf get scores by player success, found " + str(json_body.top_scores.size()) + " scores.")
 			player_scores = translate_score_fields_in_array(json_body.top_scores)
 			SWLogger.debug("Scores for " + json_body.player_name +  ": " + str(player_scores))
-			var ld_name = json_body.ld_name
-			var ld_config = json_body.ld_config
-			var player_name = json_body.player_name
 			sw_result.scores = player_scores
 		else:
 			SWLogger.error("LoudWolf get scores by player failure: " + str(json_body.error))
